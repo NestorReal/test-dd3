25dc5e5d7cf7088465d81cf6d758bac09f93ab9d
Nestor Real <nestorreal@MacBook-Pro-de-Nestor.local>
2022-02-11 18:03:31 -0600

    clean comment function helper

--- src/helpers/graphsHelpers/groupedGraph/getDataMult.tsx
+++ src/helpers/graphsHelpers/groupedGraph/getDataMult.tsx
@@ -1,14 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 import { Data } from '../../../types/graphs/groupedGraph';
 export const getDataMult = (data: Data, param: string) => Object.keys(data).map(dataInfo => data[dataInfo as keyof Data].map((item: any) => item[param]));
-
-
-// eslint-disable-next-line import/prefer-default-export
-// import { Data, VisitorsEntity } from '../../../types/graphs/groupedGraph';
-// export const getDataMult = (data: Data, param: string) => {
-//     const keys = Object.keys(data);
-//     const unformattedDataArray = keys.map(dataInfo => data[dataInfo as keyof Data]);
-//     console.log(param)
-//     const formattedData = unformattedDataArray.map((item: any) => item[param]);
-//     return formattedData;
-// };
